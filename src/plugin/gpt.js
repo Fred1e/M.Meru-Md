7dc6e4c2b0563eed78f37d213ed0660047c8bb48
FredieTech <frediezra60@gmail.com>
2024-09-10 14:18:05 +0300

    Update gpt.js

--- src/plugin/gpt.js
+++ src/plugin/gpt.js
@@ -117,7 +117,7 @@ const prompt = m.body.slice(prefix.length + cmd.length).trim();
                                     text: answer
                                 }),
                                 footer: proto.Message.InteractiveMessage.Footer.create({
-                                    text: "> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴇᴛʜɪx-ᴍᴅ"
+                                    text: "> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍ.ᴍᴇʀᴜ-ᴍᴅ"
                                 }),
                                 header: proto.Message.InteractiveMessage.Header.create({
                                     title: "",
